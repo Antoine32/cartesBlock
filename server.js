ba343ce3eb10e715470624601a2213f1fb8b8c91
Antoine32 <57961983+Antoine32@users.noreply.github.com>
2020-11-04 15:53:30 -0500

    put the setting of the ip and port in a json file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var liveServer = require("live-server");
-const readline = require('readline');
-var os = require('os');
+//const readline = require('readline');
+const network = require('./network.json');
 
-var networkInterfaces = os.networkInterfaces();
+/*var networkInterfaces = os.networkInterfaces();
 
 var choice = [];
 var names = [];
@@ -35,26 +35,26 @@ function quest() {
     rl.question('host on: ', (answer) => {
         ip = choice[parseInt(answer)];
 
-        if (ip != undefined) {
-            var params = {
-                port: 8080, // Set the server port. Defaults to 8080.
-                host: ip, // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-                root: "./webdir", // Set root directory that's being served. Defaults to cwd.
-                open: false, // When false, it won't load your browser by default.
-                ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-                file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-                wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-                mount: [['/components', './node_modules']], // Mount a directory to a route.
-                logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-                middleware: [function (req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-            };
+        if (ip != undefined) {*/
+var params = {
+    port: network.port, // Set the server port. Defaults to 8080.
+    host: network.ip, // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "./webdir", // Set root directory that's being served. Defaults to cwd.
+    open: false, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+    middleware: [function (req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
 
-            console.log("");
-            liveServer.start(params);
+//console.log("");
+liveServer.start(params);
 
-            rl.close();
-        } else {
-            quest();
-        }
-    });
+/*rl.close();
+} else {
+quest();
 }
+});
+}*/
